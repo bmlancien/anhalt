a2305f9b323360de99b5310d8d83386880b9929c
bryan.lancien <bryan.lancien@rl-institut.de>
2018-03-20 14:48:51 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
+
+// Navigation mobile
+
+let menuHamburger = document.getElementById('menu-hamburger');
+let topNavBar = document.getElementById('top-nav-bar');
+let titleBarMobile = document.getElementById('title-bar-mobile');
+
+// after loading
+titleBarMobile.style.backgroundColor = 'transparent';
+
+menuHamburger.addEventListener('click', function() {
+    // no bg color when the menu is invisible
+    if (topNavBar.style.display === 'none') {
+        titleBarMobile.style.backgroundColor = 'transparent';
+        // bg color when menu is visible
+    } else {
+        titleBarMobile.style.backgroundColor = '#002E4F';
+    }
+});
