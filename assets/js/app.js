b8d275b0153cf70767b51326f78cc8e33835ab14
bryan.lancien <bryan.lancien@rl-institut.de>
2018-05-15 14:30:57 +0200

    Base components

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,34 +10,3 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
-
-// Makes btn div position fixed or relative according to the window and form div height
-// START
-function compareHeight() {
-  let windowHeight = $( window ).height();
-  let windowWidth = $( window ).width();
-  let formHeight = $( '.scen-create__form' ).height();
-
-  console.log(windowHeight);
-  console.log(formHeight);
-
-  if (windowHeight <= formHeight - 200 && windowWidth >= 1024 || windowHeight - 300 <= formHeight && windowWidth >= 640 && windowWidth < 1024 || windowHeight - 300 <= formHeight && windowWidth < 640) {
-    $( '.scen-create__btn' ).css({ 'position': 'relative', 'width': '100%' });
-  }
-}
-
-compareHeight();
-// END 
-
-// Only for small screens
-// START
-function onSmallScreen() {
-  let windowWidth = $( window ).width();
-
-  if (windowWidth < 640) {
-    $('#nav-list-vertical').addClass('vertical');
-  }
-}
-
-onSmallScreen();
-// END
