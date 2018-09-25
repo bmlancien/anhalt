e9f650649f73d61036483634f069c93b7d3ae270
bryan.lancien <bryan.lancien@rl-institut.de>
2018-09-25 14:28:11 +0200

    added comments

--- assets/js/lib/offcanvas-expand.js
+++ assets/js/lib/offcanvas-expand.js
@@ -13,6 +13,7 @@ var offCanvasOpacity = function(op, duration) {
   $('.tabs-panel').animate({opacity: op}, duration);
 }
 
+// expands or reduces off-canvas
 $('#btnExpand').on('click', function() {
     
   if (!expanded) {
@@ -25,14 +26,18 @@ $('#btnExpand').on('click', function() {
   }
 })
 
+// makes sure that clicking on another tab will reduce the off-canvas width if the off-canvas is expanded to 100%
 $("[data-tabs]").on('change.zf.tabs', function(){
   toggleWidth('35rem', '30rem');
 });
 
+// some elements were not disappearing completely when clicking on expanded off-canvas close button (x)
+// makes sure all elements fade away
 $('#btnCloseOffcanvas').on('click', function(){
   offCanvasOpacity(0, 500);
 });
 
+// makes sure all faded elements become visible when opening off-canvas
 $('#openOffCanvas').on('click', function() {
-  offCanvasOpacity(100, 500);
+  offCanvasOpacity(1, 500);
 })
