531058c4be90263f4bc49a39d36db677b2340383
bryan.lancien <bryan.lancien@rl-institut.de>
2018-09-25 14:19:41 +0200

    changed visibility to opacity property in offcanvas-expand.js

--- assets/js/lib/offcanvas-expand.js
+++ assets/js/lib/offcanvas-expand.js
@@ -3,25 +3,36 @@ import $ from 'jquery';
 
 var expanded = false;
 
-var toggleWidth = function(offCanvasWidth, offCanvasContentWidth, visibility) {
-  $('#offCanvas').width(offCanvasWidth);
-  $('#offCanvasContent').width(offCanvasContentWidth);
-  $('#btnCloseOffcanvas').css('visibility', visibility);
+var toggleWidth = function(offCanvasWidth, offCanvasContentWidth) {
+  $('#offCanvas').animate({width: offCanvasWidth}, 500);
+  $('#offCanvasContent').animate({width: offCanvasContentWidth}, 500);
+};
+
+var offCanvasOpacity = function(op, duration) {
+  $('#offCanvas').animate({opacity: op}, duration);
+  $('.tabs-panel').animate({opacity: op}, duration);
 }
 
 $('#btnExpand').on('click', function() {
     
   if (!expanded) {
-    toggleWidth('100%', '100%', 'hidden');
+    toggleWidth('100%', '100%');
     expanded= true;
 
   } else {
-    toggleWidth('35rem', '30rem', 'visible');
+    toggleWidth('35rem', '30rem');
     expanded= false;
   }
 })
 
-$("[data-tabs]").on('change.zf.tabs', function(event){
-  toggleWidth('35rem', '30rem', 'visible');
+$("[data-tabs]").on('change.zf.tabs', function(){
+  toggleWidth('35rem', '30rem');
 });
 
+$('#btnCloseOffcanvas').on('click', function(){
+  offCanvasOpacity(0, 500);
+});
+
+$('#openOffCanvas').on('click', function() {
+  offCanvasOpacity(100, 500);
+})
