453e08d6ba979b8ffb7ccab916f4e80964058f2d
bryan.lancien <bryan.lancien@rl-institut.de>
2018-09-26 13:59:05 +0200

    expand offcanvas only for medium and large screens

--- assets/js/lib/offcanvas-expand.js
+++ assets/js/lib/offcanvas-expand.js
@@ -1,57 +1,62 @@
 import $ from 'jquery';
 
+// expand canvas only for medium and large screens
+if ($(window).width() >= 640) {
 
-var expanded = false;
+  var expanded = false;
 
-var toggleWidth = function(offCanvasWidth, offCanvasContentWidth) {
-  $('#offCanvas').animate({width: offCanvasWidth}, 500);
-  $('#offCanvasContent').animate({width: offCanvasContentWidth}, 500);
-};
+  var toggleWidth = function(offCanvasWidth, offCanvasContentWidth) {
+    $('#offCanvas').animate({width: offCanvasWidth}, 500);
+    $('#offCanvasContent').animate({width: offCanvasContentWidth}, 500);
+  };
 
-var offCanvasOpacity = function(op, duration) {
-  $('#offCanvas').animate({opacity: op}, duration);
-  $('.tabs-panel').animate({opacity: op}, duration);
-}
-
-var btnExpandText = function(expanded) {
-  if (expanded) {
-    $('#btnExpand').html('Weniger Ergebnisse <i class ="icon ion-android-remove icon--small"></i>');
-  } else {
-    $('#btnExpand').html('Mehr Ergebnisse <i class ="icon ion-android-add icon--small"></i>');
+  var offCanvasOpacity = function(op, duration) {
+    $('#offCanvas').animate({opacity: op}, duration);
+    $('.tabs-panel').animate({opacity: op}, duration);
   }
-}
 
-// expands or reduces off-canvas width
-$('#btnExpand').on('click', function() {
-    
-  if (!expanded) {
-    toggleWidth('100%', '100%');
-    expanded= true;
+  var btnExpandText = function(expanded) {
+    if (expanded) {
+      $('#btnExpand').html('Weniger Ergebnisse <i class ="icon ion-android-remove icon--small"></i>');
+    } else {
+      $('#btnExpand').html('Mehr Ergebnisse <i class ="icon ion-android-add icon--small"></i>');
+    }
+  }
 
-  } else {
+  // expands or reduces off-canvas width
+  $('#btnExpand').on('click', function() {
+      
+    if (!expanded) {
+      toggleWidth('100%', '100%');
+      expanded= true;
+
+    } else {
+      toggleWidth('35rem', '30rem');
+      expanded= false;
+    }
+
+    // changes btn text
+    btnExpandText(expanded);
+  })
+
+  // makes sure that clicking on another tab will reduce the off-canvas width if the off-canvas is expanded to 100%
+  $("[data-tabs]").on('change.zf.tabs', function(){
     toggleWidth('35rem', '30rem');
-    expanded= false;
-  }
 
-  // changes btn text
-  btnExpandText(expanded);
-})
+    // changes btn text if needed
+    btnExpandText(false);
+  });
 
-// makes sure that clicking on another tab will reduce the off-canvas width if the off-canvas is expanded to 100%
-$("[data-tabs]").on('change.zf.tabs', function(){
-  toggleWidth('35rem', '30rem');
+  // some elements were not disappearing completely when clicking on expanded off-canvas close button (x)
+  // makes sure all elements fade away
+  $('#btnCloseOffcanvas').on('click', function(){
+    offCanvasOpacity(0, 500);
+  });
 
-  // changes btn text if needed
-  btnExpandText(false);
-});
+  // makes sure all faded elements become visible when opening off-canvas
+  $('#openOffCanvas').on('click', function() {
+    offCanvasOpacity(1, 500);
+  })
+}
 
-// some elements were not disappearing completely when clicking on expanded off-canvas close button (x)
-// makes sure all elements fade away
-$('#btnCloseOffcanvas').on('click', function(){
-  offCanvasOpacity(0, 500);
-});
 
-// makes sure all faded elements become visible when opening off-canvas
-$('#openOffCanvas').on('click', function() {
-  offCanvasOpacity(1, 500);
-})
