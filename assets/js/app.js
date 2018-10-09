4f478a96c804621992e0e43a8bc83a02bc18dcb7
bryan.lancien <bryan.lancien@rl-institut.de>
2018-10-09 14:16:54 +0200

    removed highcharts test

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,6 @@ import './lib/jquery-ui';
 import './lib/leaflet-map';
 import './lib/offcanvas-expand';
 import './lib/radio-btn-checked';
-import './lib/highcharts-results';
 
 import './lib/ion-range-slider';
 
