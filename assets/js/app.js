53ca83dd8b434463ccd6a31c74b0725f2676ce76
bryan.lancien <bryan.lancien@rl-institut.de>
2018-10-09 11:05:56 +0200

    included highcharts test

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ import './lib/jquery-ui';
 import './lib/leaflet-map';
 import './lib/offcanvas-expand';
 import './lib/radio-btn-checked';
+import './lib/highcharts-results';
 
 import './lib/ion-range-slider';
 
---
+++ assets/js/lib/highcharts-results.js
@@ -0,0 +1,53 @@
+import $ from 'jquery';
+
+$(function () { 
+    var myChart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
+
+$(function () { 
+    var myChart = Highcharts.chart('container2', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Another Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
