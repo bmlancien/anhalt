745e34f9672b01f3e27f9de6dd3de918b6cafc45
bryan.lancien <bryan.lancien@rl-institut.de>
2018-11-08 10:37:51 +0100

    improved jquery code in highlight-tab.js

--- assets/js/lib/highlight-tab.js
+++ assets/js/lib/highlight-tab.js
@@ -3,7 +3,11 @@ import $ from 'jquery';
 // after simulation is done
 // replace click event with appropriate event
 $('#simulation-btn').on('click', function() {
-  $('#tabsResults > a').addClass('results-highlighted');
+
+  // if user has already results panel open
+  if (!$('#tabsResults').is('.is-active')) {
+    $('#tabsResults > a').addClass('results-highlighted');
+  }
 });
 
 // when user clicks on results tab
